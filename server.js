d3e9e7f1fe2295d5ad6f662cae2e08d79c3cc46d
Clint Brodar <clintbrodar@gmail.com>
2020-07-26 16:50:22 -0700

    Allow CORS

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express = require("express");
 require('dotenv').config();
 const PORT = process.env.PORT || 3000;
 const app = express();
+const cors = require("cors");
 const routes = require("./routes");
 
+// enable all CORS requests
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
